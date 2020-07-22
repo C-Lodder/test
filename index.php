154e6a3da3f0b3526f89a31c8f82e871d4754eaa
Lodder <lodder15@hotmail.co.uk>
2020-07-22 15:03:35 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,193 @@
+<!DOCTYPE html>
+<html lang="en-gb">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="author" content="Charlie Lodder">
+	<meta name="description" content="Lookleft">
+	<meta name="theme-color" content="#fff">
+	<title>Lookleft</title>
+	<link rel="preload" href="css/style.min" as="style">
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+	<section class="banner">
+		<div class="logo">lookleft</div>
+	</section>
+
+	<section>
+		<div class="container">
+			<h1 class="text-center"><span class="text-primary">LOOK LEFT</span> IS NOT ANOTHER AGENCY.<br>IT'S A <span class="text-primary">COLLECTIVE</span></h1>
+			<p class="text-center">We're rulebreakers who believe in a fresh approach to producing events.</p>
+		</div>
+	</section>
+
+	<section class="bg-primary-light text-secondary">
+		<div class="container">
+			<div class="row">
+				<div class="col-6">
+					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 535.417 535.417"><path fill="#fff" d="M500.365 167.693c-9.12-44.77-3.293-138.514-158.295-163.234C187.062-20.285 47.398 58.361 82.529 230.248c0 31.33-62.035 79.965-47.088 96.406 14.946 16.441 40.31 17.083 40.31 17.083s-16.5 31.33-9.073 39.982c7.409 8.653 28.025 4.951 28.025 4.951s-19.156 13.803-19.156 24.511c0 10.72 32.345 24.954 32.345 24.954s-5.932 59.285 42.64 55.174c48.57-4.109 56.75-17.83 56.75-17.83l14.398 59.938h188l-6.621-130.27c0 .012 115.452-125.318 97.306-237.454zm-142.601 92.869c3.854-7.077 4.975-15.636 2.394-23.927-5.079-16.254-22.35-25.328-38.616-20.271-16.254 5.056-25.316 22.35-20.271 38.604 2.604 8.291 8.361 14.701 15.565 18.333l4.099 13.172-68.813 21.428-26.507-85.092-10.76 3.352c-4.233 8.337-11.66 15.052-21.299 18.053-18.87 5.885-38.919-4.647-44.793-23.518-5.879-18.87 4.665-38.932 23.523-44.805 9.634-2.989 19.571-1.705 27.763 2.768l10.772-3.352-23.734-76.228 68.825-21.427 3.24 10.404c-3.859 7.064-4.975 15.612-2.388 23.915 5.056 16.243 22.332 25.328 38.599 20.26 16.254-5.068 25.339-22.35 20.271-38.604-2.593-8.302-8.361-14.713-15.554-18.345l-3.247-10.404 70.18-21.848 24.919 80.035-7.274 2.265c-7.064-3.865-15.636-4.986-23.927-2.394-16.255 5.056-25.339 22.338-20.271 38.593s22.351 25.339 38.616 20.271c8.279-2.592 14.701-8.361 18.321-15.554l7.286-2.254 27.359 87.87-70.167 21.848-4.111-13.148z"/></svg>
+				</div>
+				<div class="col-6">
+					<h2>COLLABORATIVE,<br>AGILE,<br>CREATIVE</h1>
+					<p>Based in London at the heart of a global network, Look Left curates the most talented individuals and innovative suppliers from across the industry to build a bespoke team and approach for every event.</p>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<section class="p-0">
+		<div class="container">
+			<div class="row">
+				<div class="col-4 bg-secondary">
+					<div class="py-4">
+						<h2 class="text-light text-end">HOW<br>WE<br>WORK</h1>
+					</div>
+				</div>
+				<div class="col-6">
+					<div class="py-4">
+						<p>[INTERACTIVE CUSTOM ELEMENT]</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section> 
+
+	<section>
+		<div class="container">
+			<h2 class="text-primary">SERVICES</h2>
+			<div class="row">
+				<div class="col-6">
+					<p>Our talented teams are ready to get hands-on with whatever your brief requires.</p>
+					<p>Here's a snapshot of some of the types of events our experience covers:</p>
+					<p class="text-center"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" width="50%"><path fill="var(--primary-light)" d="M482.696 299.276l-32.61-18.827a195.168 195.168 0 0 0 0-48.899l32.61-18.827c9.576-5.528 14.195-16.902 11.046-27.501-11.214-37.749-31.175-71.728-57.535-99.595-7.634-8.07-19.817-9.836-29.437-4.282l-32.562 18.798a194.125 194.125 0 0 0-42.339-24.48V38.049c0-11.13-7.652-20.804-18.484-23.367-37.644-8.909-77.118-8.91-114.77 0-10.831 2.563-18.484 12.236-18.484 23.367v37.614a194.101 194.101 0 0 0-42.339 24.48L105.23 81.345c-9.621-5.554-21.804-3.788-29.437 4.282-26.36 27.867-46.321 61.847-57.535 99.595-3.149 10.599 1.47 21.972 11.046 27.501l32.61 18.827a195.168 195.168 0 0 0 0 48.899l-32.61 18.827c-9.576 5.528-14.195 16.902-11.046 27.501 11.214 37.748 31.175 71.728 57.535 99.595 7.634 8.07 19.817 9.836 29.437 4.283l32.562-18.798a194.08 194.08 0 0 0 42.339 24.479v37.614c0 11.13 7.652 20.804 18.484 23.367 37.645 8.909 77.118 8.91 114.77 0 10.831-2.563 18.484-12.236 18.484-23.367v-37.614a194.138 194.138 0 0 0 42.339-24.479l32.562 18.798c9.62 5.554 21.803 3.788 29.437-4.283 26.36-27.867 46.321-61.847 57.535-99.595 3.149-10.599-1.47-21.972-11.046-27.501zm-65.479 100.461l-46.309-26.74c-26.988 23.071-36.559 28.876-71.039 41.059v53.479a217.145 217.145 0 0 1-87.738 0v-53.479c-33.621-11.879-43.355-17.395-71.039-41.059l-46.309 26.74c-19.71-22.09-34.689-47.989-43.929-75.958l46.329-26.74c-6.535-35.417-6.538-46.644 0-82.079l-46.329-26.74c9.24-27.969 24.22-53.869 43.929-75.969l46.309 26.76c27.377-23.434 37.063-29.065 71.039-41.069V44.464a216.79 216.79 0 0 1 87.738 0v53.479c33.978 12.005 43.665 17.637 71.039 41.069l46.309-26.76c19.709 22.099 34.689 47.999 43.929 75.969l-46.329 26.74c6.536 35.426 6.538 46.644 0 82.079l46.329 26.74c-9.24 27.968-24.219 53.868-43.929 75.957zM256 160c-52.935 0-96 43.065-96 96s43.065 96 96 96 96-43.065 96-96-43.065-96-96-96zm0 160c-35.29 0-64-28.71-64-64s28.71-64 64-64 64 28.71 64 64-28.71 64-64 64z"/></svg></p>
+				</div>
+				<div class="col-6">
+					<ul>
+						<li>Product launches</li>
+						<li>Corporate brand celebrations</li>
+						<li>Press, influencer &amp; industry events</li>
+						<li>In-store and online retailer experiences</li>
+						<li>Road shows</li>
+						<li>PR Stunts</li>
+						<li>Festival activations</li>
+						<li>Award ceremonies</li>
+						<li>Conferences</li>
+						<li>Workshops</li>
+						<li>Incentive trips and overseas brand activations</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<section class="bg-secondary text-light">
+		<div class="container">
+			<div class="row">
+				<div class="col-6">
+					<h2 class="logo text-end">lookleft</h2>
+				</div>
+				<div class="col-6">
+					<h2>IS A COLLECTIVE</h2>
+					<p>The Look Left Collective is an unrivalled global network of the very best producers, suppliers and creatives the event industry has to offer.</p>
+					<p>Head of Events and Look Left Founder, Claire Douglas-Green, hand-picks your bespoke team from the collective, bringing together an agile, talented task-force of the brightest in the business.</p>
+					<p>[IMAGE WITH PEOPLE]</p>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<section>
+		<div class="container">
+			<div class="row">
+				<div class="col-4">
+					<h2 class="text-primary">CAREERS</h2>
+					<p>We're always looking for talented people to join our collective.</p>
+					<p>Click here to send in your CV and we'll be in touch should we need your support.</p>
+					<p><a href="mailto:letschat@lookleft.london" target="_self" class="btn">CONTACT US</a></p>
+				</div>
+				<div class="col-8 text-secondary text-end">
+					<svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512" width="70%"><path fill="currentColor" d="M544 224c44.2 0 80-35.8 80-80s-35.8-80-80-80-80 35.8-80 80 35.8 80 80 80zm0-128c26.5 0 48 21.5 48 48s-21.5 48-48 48-48-21.5-48-48 21.5-48 48-48zM320 256c61.9 0 112-50.1 112-112S381.9 32 320 32 208 82.1 208 144s50.1 112 112 112zm0-192c44.1 0 80 35.9 80 80s-35.9 80-80 80-80-35.9-80-80 35.9-80 80-80zm244 192h-40c-15.2 0-29.3 4.8-41.1 12.9 9.4 6.4 17.9 13.9 25.4 22.4 4.9-2.1 10.2-3.3 15.7-3.3h40c24.2 0 44 21.5 44 48 0 8.8 7.2 16 16 16s16-7.2 16-16c0-44.1-34.1-80-76-80zM96 224c44.2 0 80-35.8 80-80s-35.8-80-80-80-80 35.8-80 80 35.8 80 80 80zm0-128c26.5 0 48 21.5 48 48s-21.5 48-48 48-48-21.5-48-48 21.5-48 48-48zm304.1 180c-33.4 0-41.7 12-80.1 12-38.4 0-46.7-12-80.1-12-36.3 0-71.6 16.2-92.3 46.9-12.4 18.4-19.6 40.5-19.6 64.3V432c0 26.5 21.5 48 48 48h288c26.5 0 48-21.5 48-48v-44.8c0-23.8-7.2-45.9-19.6-64.3-20.7-30.7-56-46.9-92.3-46.9zM480 432c0 8.8-7.2 16-16 16H176c-8.8 0-16-7.2-16-16v-44.8c0-16.6 4.9-32.7 14.1-46.4 13.8-20.5 38.4-32.8 65.7-32.8 27.4 0 37.2 12 80.2 12s52.8-12 80.1-12c27.3 0 51.9 12.3 65.7 32.8 9.2 13.7 14.1 29.8 14.1 46.4V432zM157.1 268.9c-11.9-8.1-26-12.9-41.1-12.9H76c-41.9 0-76 35.9-76 80 0 8.8 7.2 16 16 16s16-7.2 16-16c0-26.5 19.8-48 44-48h40c5.5 0 10.8 1.2 15.7 3.3 7.5-8.5 16.1-16 25.4-22.4z"/></svg>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<footer>
+		<div class="container">
+			<div class="row">
+				<div class="col-7">
+					<img src="img/footer.webp" alt="" loading="lazy">
+				</div>
+				<div class="col-5 text-light">
+					<div class="flex flex-column text-end justify-content-between h-100 py-4">
+						<div>
+							<h2>WHERE TO<br>FIND US</h1>
+						</div>
+						<div>
+							<p>
+								<a href="mailto:letschat@lookleft.london" target="_self">letschat@lookleft.london<svg class="ml-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" width="1em"><path fill="currentColor" d="M464 64H48C21.5 64 0 85.5 0 112v288c0 26.5 21.5 48 48 48h416c26.5 0 48-21.5 48-48V112c0-26.5-21.5-48-48-48zM48 96h416c8.8 0 16 7.2 16 16v41.4c-21.9 18.5-53.2 44-150.6 121.3-16.9 13.4-50.2 45.7-73.4 45.3-23.2.4-56.6-31.9-73.4-45.3C85.2 197.4 53.9 171.9 32 153.4V112c0-8.8 7.2-16 16-16zm416 320H48c-8.8 0-16-7.2-16-16V195c22.8 18.7 58.8 47.6 130.7 104.7 20.5 16.4 56.7 52.5 93.3 52.3 36.4.3 72.3-35.5 93.3-52.3 71.9-57.1 107.9-86 130.7-104.7v205c0 8.8-7.2 16-16 16z"/></svg></a>
+							</p>
+							<p>
+								<a href="tel:+44 (0)7944669207">+44 (0)7944669207<svg class="ml-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" width="1em"><path fill="currentColor" d="M493.09 351.3L384.7 304.8a31.36 31.36 0 0 0-36.5 8.9l-44.1 53.9A350 350 0 0 1 144.5 208l53.9-44.1a31.35 31.35 0 0 0 8.9-36.49l-46.5-108.5A31.33 31.33 0 0 0 125 .81L24.2 24.11A31.05 31.05 0 0 0 0 54.51C0 307.8 205.3 512 457.49 512A31.23 31.23 0 0 0 488 487.7L511.19 387a31.21 31.21 0 0 0-18.1-35.7zM456.89 480C222.4 479.7 32.3 289.7 32.1 55.21l99.6-23 46 107.39-72.8 59.5C153.3 302.3 209.4 358.6 313 407.2l59.5-72.8 107.39 46z"/></svg></a>
+							</p>
+							<p>
+								<a href="https://instagram.com/lookleft.london" target="_blank" rel="noopener">lookleft.london<svg class="ml-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" width="1em"><path fill="currentColor" d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z"/></svg></a>
+							</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</footer>
+	
+	
+	<script>
+		const username = 'lodder'
+		const password = 'test'
+		
+		let headers = new Headers()
+		headers.set('Authorization', 'Basic ' + btoa(username + ':' + password))
+		headers.set('Content-Type', 'text/plain')
+
+		fetch('https://cors.joomjunk.co.uk', {
+			credentials: 'include',
+			method: 'GET',
+			headers: headers
+		})
+			.then(response => response.text())
+			.then(response => new DOMParser().parseFromString(response, 'text/html'))
+			.then(data => {
+				console.log(data.querySelectorAll('pre a'))
+			})
+	</script>
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+</body>
+</html>
